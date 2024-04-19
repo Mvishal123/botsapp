2e0f6372d7e3958d96c6a7edfda0e157e11fa875
vishal <vishalmahesh.study@gmail.com>
2024-04-19 23:26:13 +0530

    ui: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/pages/landing";
 import Header from "./components/header";
+import Footer from "./components/footer";
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
+      <footer className="mt-20">
+        <Footer />
+      </footer>
     </BrowserRouter>
   );
 };
---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="section-spacing py-12 bg-brand/secondary">
+      <div>
+        <h1 className="text-2xl font-bolotta text-brand/green-medium">
+          Botsapp
+        </h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
