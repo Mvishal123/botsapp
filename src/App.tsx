7b0a90f3efc947f5f0946c1907f46088b51e6fb8
vishal <vishalmahesh.study@gmail.com>
2024-04-19 09:39:00 +0530

    ui :hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { gradient1, mainbot, swirl } from "./assets";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl text-brand/green-dark">Hye</h1>
+    <div className="h-screen overflow-hidden">
+      <div className="flex justify-center items-center md:items-end h-full relative">
+        <img
+          src={mainbot}
+          alt="whatsapp bot"
+          height={550}
+          width={550}
+          className="z-10 h-[400px] md:h-[550px] w-[400px] md:w-[550px]"
+        />
+        <img
+          src={swirl}
+          alt="swirl"
+          width={700}
+          height={700}
+          className="absolute"
+        />
+        <img
+          src={gradient1}
+          alt="swirl"
+          width={1100}
+          height={1200}
+          className="absolute blur-3xl opacity-30 -rotate-45"
+        />
+      </div>
     </div>
   );
 };
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,10 @@
-import grainy from "../../public/svg/grainy.svg";
-import swirl from "../../public/svg/swirl.svg";
+import grainy from "/svg/grainy.svg";
+import swirl from "/svg/swirl.svg";
+import gradient1 from "/svg/gradient1.svg";
 
 // images
-import mainbot from "../../public/images/mainbot.png";
-import bot1 from "../../public/images/bot1.png";
-import bot2 from "../../public/images/bot2.png";
+import mainbot from "/images/mainbot.png";
+import bot1 from "/images/bot1.png";
+import bot2 from "/images/bot2.png";
 
-export { grainy, swirl, mainbot, bot1, bot2 };
+export { grainy, swirl, mainbot, bot1, bot2, gradient1};
