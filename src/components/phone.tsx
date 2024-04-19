77e25d60fa64ed3ae5b5ed13e52cfac149ad7a2a
vishal <vishalmahesh.study@gmail.com>
2024-04-19 17:19:26 +0530

    fix -ui: overflow of whatsapp chat img

--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -1,20 +1,23 @@
 import { phone } from "../assets";
 
+
 interface PhoneProps {
   chatImg: string;
   className?: string;
 }
 const Phone = ({ chatImg, className = "" }: PhoneProps) => {
   return (
-    <div
-      className={`relative ${className}`}
-    >
-      <img src={phone} alt="phone" className="w-[250px] md:w-[200px] lg:w-[300px]" />
+    <div className={`relative overflow-hidden ${className}`}>
+      <img
+        src={phone}
+        alt="phone"
+        className="w-[250px] md:w-[200px] lg:w-[400px]"
+      />
       <div className="flex justify-center items-center">
         <img
           src={chatImg}
           alt=""
-          className="absolute top-2 -z-10 w-[90%] h-[97%] object-cover rounded-2xl"
+          className="absolute top-2 xl:top-4 -z-10 w-[90%] h-[96%] object-cover rounded-2xl"
         />
       </div>
     </div>
